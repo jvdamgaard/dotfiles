166e41f7889792de8ae8a1b919bc5dcaad82439b
Jakob Viskum Damgaard <jakob.viskum.damgaard@gmail.com>
2015-04-07 21:42:03 +0200

    sudo upfront

--- .init/start.sh
+++ .init/start.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+#
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until script has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Install ruby
 \curl -sSL https://get.rvm.io | bash -s stable --ruby
